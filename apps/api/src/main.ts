deb87b54d350f9210e28b5ba5c02a05b32673892
MateusSales02 <salesmateus463@gmail.com>
2025-11-21 13:35:02 -0300

    fix: bind api on 0.0.0.0

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   app.enableCors({
     origin: ['http://localhost:5173', 'http://54.161.67.120'],
     credentials: true,
   });
-  await app.listen(3000, '0.0.0.0');
-
+  await app.listen(port);
 }
 bootstrap();
