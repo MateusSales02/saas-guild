bb37a849bf7e591401109bf839a636c2dbdc233c
MateusSales02 <salesmateus463@gmail.com>
2025-11-22 03:05:49 -0300

    Adiciona app.listen no bootstrap da API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://54.161.67.120', // frontend em produção (porta 80)
-    ],
+    origin: true, // libera tudo por enquanto (TCC/dev)
     credentials: true,
   });
+
+  await app.listen(port, '0.0.0.0');
+  Logger.log(`API ouvindo em http://0.0.0.0:${port}`, 'Bootstrap');
 }
 bootstrap();
